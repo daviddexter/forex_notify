19fdb9169eb718e71c4f09add63ac091b2a85505
DavidDexter <dmwangi@kineticengines.co.ke>
2020-11-30 04:20:53 +0300

    feat : add http client
    Signed-off-by: DavidDexter <dmwangi@kineticengines.co.ke>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+
+use std::io::prelude::*;
+use std::process;
+use std::thread;
+use std::time::Duration;
+use std::{fs::create_dir, fs::File, io::Write, path::PathBuf};
+
+extern crate job_scheduler;
+use job_scheduler::{Job, JobScheduler};
 
 use dirs::config_dir;
+
+extern crate reqwest;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Config {
@@ -11,9 +21,13 @@ struct Config {
 }
 
 fn main() {
+    println!("started forex_notify");
+
     let config_name: &str = "forex_notify/config.yaml";
 
-    println!("started forex_notify. Checking is config file exists");
+    thread::sleep(Duration::from_millis(1000));
+
+    println!("retrieving config file");
 
     let config_path = {
         let o = config_dir().unwrap();
@@ -28,8 +42,10 @@ fn main() {
 }
 
 fn create_defualt_config(p: PathBuf) {
-    // create the config then pass it to listen
+    println!("cound not find config file. creating default one");
 
+    let org = p.clone();
+    // create the config then pass it to listen fn
     let config: Config = toml::from_str(
         r#"
         major_currency = 'USD'
@@ -43,9 +59,60 @@ fn create_defualt_config(p: PathBuf) {
     assert_eq!(config.minor_currency, "KES");
     assert_eq!(config.poll_frequency, 2);
 
-    listen(p);
+    println!("{:?}", p);
+
+    let dir = p.parent().unwrap();
+    create_dir(dir).expect("failed to create config directory");
+
+    let mut file = File::create(p.to_str().unwrap()).expect("error while creating config file");
+
+    let tml = toml::to_string(&config).unwrap();
+
+    file.write(tml.as_bytes())
+        .expect("could not write to config toml file");
+
+    listen(org);
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ApiResp {
+    code: String,
+    rate: String,
 }
 
 fn listen(p: PathBuf) {
-    println!("path is {:?}", p);
+    println!("config file found at {:?}. setting up client", p);
+
+    let mut file = File::open(p.to_str().unwrap()).expect("failed to open config file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("failed to read config file");
+
+    let config: Config = toml::from_str(&contents).unwrap();
+
+    if config.poll_frequency < 1 || config.poll_frequency > 23 {
+        println!(
+            "Invalid poll_frequency. Expected to range 1 - 23. Found {}",
+            config.poll_frequency
+        );
+        process::exit(0);
+    }
+
+    let tick = format!("0 */{} * * * *", config.poll_frequency);
+
+    let mut sched = JobScheduler::new();
+
+    sched.add(Job::new(tick.parse().unwrap(), || {
+        // let resp =
+        //     reqwest::blocking::get("https://httpbin.org/ip")?.json::<HashMap<String, String>>()?;
+        // println!("{:#?}", resp);
+
+        let resp = reqwest::blocking::get("https://httpbin.org/ip").expect("");
+        println!("{:?}", resp);
+    }));
+
+    loop {
+        sched.tick();
+        std::thread::sleep(Duration::from_millis(100));
+    }
 }
